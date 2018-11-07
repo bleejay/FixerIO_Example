37da4fad7ba042fa9b88e0dc59dd86004988d75e
Bleejay <jasonblee@aol.com>
2018-11-07 11:30:25 +0000

    initial commit

---
+++ src/test/java/FixerIO_Example/AppTest.java
@@ -0,0 +1,20 @@
+package FixerIO_Example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
