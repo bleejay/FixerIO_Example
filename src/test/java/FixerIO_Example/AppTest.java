0f1e00e3d5e19c96fff384ef7dfeb4131a3f5275
Bleejay <jasonblee@aol.com>
2018-11-07 14:42:11 +0000

    added test file with basic tests for application

--- src/test/java/FixerIO_Example/AppTest.java
+++ src/test/java/FixerIO_Example/AppTest.java
@@ -2,19 +2,44 @@ package FixerIO_Example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    FixerLatestRatesParser ratesParser = new FixerLatestRatesParser();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSuccess(){
+        Assert.assertEquals(true, ratesParser.getFileSuccess());
+    }
+
+//    @Test
+//    public void testTimeStamp(){
+//        Assert.assertEquals(1539182646, ratesParser.getTimeStamp());
+//    }
+
+    @Test
+    public void testBaseRate(){
+        Assert.assertEquals("EUR", ratesParser.getBaseRate());
+    }
+
+//    @Test
+//    public void testDate(){
+//        Assert.assertEquals("2018-10-10", ratesParser.getDate());
+//    }
+
+    @Test
+    public void testNumberOfRates(){
+        Assert.assertEquals(168,ratesParser.getRates().size());
+    }
+
+    @Test
+    public void testSpecificRate(){
+        System.out.println(ratesParser.getSpecificRate("EUR"));
     }
 }
+
